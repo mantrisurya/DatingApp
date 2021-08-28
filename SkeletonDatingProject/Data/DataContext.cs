f74d36b089ea8b73b1af69772e39bc72ae04562b
mantri.surya <mantri.surya.1@gmail.com>
2021-08-28 21:51:14 +0530

    Section 2

---
+++ SkeletonDatingProject/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SkeletonDatingProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkeletonDatingProject.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        { }
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
